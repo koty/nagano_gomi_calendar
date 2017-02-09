214a5ea5cb51e5b0823b09f669f5326600406055
koty <kouichi.nishizawa+github@gmail.com>
2017-02-09 14:29:48 +0900

    * 不燃と枝葉の収集日程が逆になっていたのを修正 * 年度を正しく修正

--- parse_xls.py
+++ parse_xls.py
@@ -11,12 +11,21 @@ def read_column_as_date(book, sheet, column_index, kind):
         c = sheet.cell(i, column_index)
         if not c.value:
             break
+        now = datetime.datetime.now()
+        current_year = now.year - 1 if now.month <= 3 else now.year
+        # 29年度番のカレンダーだったので、一年足す
+        current_year += 1
         if isinstance(c.value, str):
             month_day = c.value.replace('○', '').replace('日', '').split('月')
-            d = datetime.datetime.now()
-            d = d.replace(month=int(month_day[0]), day=int(month_day[1]))
+            month = int(month_day[0])
+            day = int(month_day[1])
         else:
-            d = datetime.datetime(*xlrd.xldate_as_tuple(c.value, book.datemode))
+            date = datetime.datetime(*xlrd.xldate_as_tuple(c.value, book.datemode))
+            day = date.day
+            month = date.month
+        # yearは入力時点のものがはいっているので、置き換える
+        year = current_year + 1 if month <= 3 else current_year
+        d = datetime.datetime(year=year, month=month, day=day)
         calendar_list.append({'date': d.strftime('%Y-%m-%d'), 'kind': kind})
         i += 1
 
@@ -38,8 +47,8 @@ def parse_as_object(path):
         calendar_list.extend(read_column_as_date(book, sheet, 4, '可燃'))
         calendar_list.extend(read_column_as_date(book, sheet, 5, 'プラ'))
         calendar_list.extend(read_column_as_date(book, sheet, 6, 'プラ'))
-        calendar_list.extend(read_column_as_date(book, sheet, 7, '不燃'))
-        calendar_list.extend(read_column_as_date(book, sheet, 8, '枝葉'))
+        calendar_list.extend(read_column_as_date(book, sheet, 7, '枝葉'))
+        calendar_list.extend(read_column_as_date(book, sheet, 8, '不燃'))
         calendar_list.extend(read_column_as_date(book, sheet, 9, '紙ペ'))
         calendar_list.extend(read_column_as_date(book, sheet, 10, '缶ペ'))
         calendar_list.extend(read_column_as_date(book, sheet, 11, 'ビン'))
